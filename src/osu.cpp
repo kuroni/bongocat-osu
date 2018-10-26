1d497afae14228b373d3e2f68ace4f2a41c7da1b
Trung Dang <bill081001@gmail.com>
2018-10-26 17:11:00 +0700

    Fixed aim arm overlaps smoke (#3)

--- src/osu.cpp
+++ src/osu.cpp
@@ -154,83 +154,6 @@ void draw()
         y = -76 * fx - 40 * fy + 324;
     }
     window.draw(bg);
-    
-    // drawing smoke
-    for (Json::Value &v : smoke_key_value)
-        if (GetKeyState(v.asInt()) & 0x8000)
-        {
-            window.draw(smoke);
-            break;
-        }
-
-    // drawing keypresses
-    bool left_key = false;
-    for (Json::Value &v : left_key_value)
-        if (GetKeyState(v.asInt()) & 0x8000)
-        {
-            left_key = true;
-            break;
-        }
-    if (left_key)
-    {
-        if (!left_key_state)
-        {
-            key_state = 1;
-            left_key_state = true;
-        }
-    }
-    else
-        left_key_state = false;
-
-    bool right_key = false;
-    for (Json::Value &v : right_key_value)
-        if (GetKeyState(v.asInt()) & 0x8000)
-        {
-            right_key = true;
-            break;
-        }
-    if (right_key)
-    {
-        if (!right_key_state)
-        {
-            key_state = 2;
-            right_key_state = true;
-        }
-    }
-    else
-        right_key_state = false;
-
-    if (!left_key_state && !right_key_state)
-    {
-        key_state = 0;
-        window.draw(up);
-    }
-    if (key_state == 1)
-    {
-        if ((clock() - timer_right_key) / CLOCKS_PER_SEC > 0.031)
-        {
-            if (!is_left_handed)
-                window.draw(left);
-            else
-                window.draw(right);
-            timer_left_key = clock();
-        }
-        else
-            window.draw(up);
-    }
-    else if (key_state == 2)
-    {
-        if ((clock() - timer_left_key) / CLOCKS_PER_SEC > 0.031)
-        {
-            if (!is_left_handed)
-                window.draw(right);
-            else
-                window.draw(left);
-            timer_right_key = clock();
-        }
-        else
-            window.draw(up);
-    }
 
     // initializing pss and pss2 (kuvster's magic)
     int oof = 6;
@@ -386,6 +309,83 @@ void draw()
     circ2.setRadius(width / 2);
     circ2.setPosition(pss2[50] - width / 2, pss2[51] - width / 2);
     window.draw(circ2);
+    
+    // drawing smoke
+    for (Json::Value &v : smoke_key_value)
+        if (GetKeyState(v.asInt()) & 0x8000)
+        {
+            window.draw(smoke);
+            break;
+        }
+
+    // drawing keypresses
+    bool left_key = false;
+    for (Json::Value &v : left_key_value)
+        if (GetKeyState(v.asInt()) & 0x8000)
+        {
+            left_key = true;
+            break;
+        }
+    if (left_key)
+    {
+        if (!left_key_state)
+        {
+            key_state = 1;
+            left_key_state = true;
+        }
+    }
+    else
+        left_key_state = false;
+
+    bool right_key = false;
+    for (Json::Value &v : right_key_value)
+        if (GetKeyState(v.asInt()) & 0x8000)
+        {
+            right_key = true;
+            break;
+        }
+    if (right_key)
+    {
+        if (!right_key_state)
+        {
+            key_state = 2;
+            right_key_state = true;
+        }
+    }
+    else
+        right_key_state = false;
+
+    if (!left_key_state && !right_key_state)
+    {
+        key_state = 0;
+        window.draw(up);
+    }
+    if (key_state == 1)
+    {
+        if ((clock() - timer_right_key) / CLOCKS_PER_SEC > 0.031)
+        {
+            if (!is_left_handed)
+                window.draw(left);
+            else
+                window.draw(right);
+            timer_left_key = clock();
+        }
+        else
+            window.draw(up);
+    }
+    else if (key_state == 2)
+    {
+        if ((clock() - timer_left_key) / CLOCKS_PER_SEC > 0.031)
+        {
+            if (!is_left_handed)
+                window.draw(right);
+            else
+                window.draw(left);
+            timer_right_key = clock();
+        }
+        else
+            window.draw(up);
+    }
 
     // drawing tablet
     if (!is_mouse)
