e5e6395b9541be4739c7fa4af73a11436d62a0c3
Trung Dang <bill081001@gmail.com>
2020-08-13 15:31:38 -0400

    Fix keycodes (#71)

--- src/input.cpp
+++ src/input.cpp
@@ -1,71 +1,79 @@
 #include "header.hpp"
 #include <SFML/Window.hpp>
 
-#define TOTAl_INPUT_TABLE_SIZE 128
+#define TOTAl_INPUT_TABLE_SIZE 256
 
 namespace input {
 
 int INPUT_KEY_TABLE[TOTAl_INPUT_TABLE_SIZE];
 
 void init() {
-    for (int i = 0; i < TOTAl_INPUT_TABLE_SIZE; ++i) {
-
-        bool is_number = (i >= 48 && i <= 57);
-        bool is_uppercase_eng_alphabet = (i >= 65 && i <= 90);
-        bool is_lowercase_eng_alphabet = (i >= 97 && i <= 122);
-
-        if (is_number) {
-            INPUT_KEY_TABLE[i] = (i - 22);
-        }
-        else if (is_uppercase_eng_alphabet) {
-            INPUT_KEY_TABLE[i] = (i - 65);
-        }
-        else if (is_lowercase_eng_alphabet) {
-            INPUT_KEY_TABLE[i] = (i - 97);
-        }
-        else {
-            INPUT_KEY_TABLE[i] = -1;
+    for (int i = 0; i < TOTAl_INPUT_TABLE_SIZE; i++) {
+        if (i >= 48 && i <= 57) {           // number
+            INPUT_KEY_TABLE[i] = i - 48 + (int)sf::Keyboard::Key::Num0;
+        } else if (i >= 65 && i <= 90) {    // english alphabet
+            INPUT_KEY_TABLE[i] = i - 65 + (int)sf::Keyboard::Key::A;
+        } else if (i >= 96 && i <= 105) {   // numpad
+            INPUT_KEY_TABLE[i] = i - 96 + (int)sf::Keyboard::Key::Numpad0;
+        } else if (i >= 112 && i <= 126) {  // function
+            INPUT_KEY_TABLE[i] = i - 112 + (int)sf::Keyboard::Key::F1;
+        } else {
+            INPUT_KEY_TABLE[i] = (int)sf::Keyboard::Key::Unknown;
         }
     }
 
-    INPUT_KEY_TABLE[8] = (int)(sf::Keyboard::Key::Backspace);
-    INPUT_KEY_TABLE[9] = (int)(sf::Keyboard::Key::Tab);
-    INPUT_KEY_TABLE[11] = (int)(sf::Keyboard::Key::Tab);
-    INPUT_KEY_TABLE[13] = (int)(sf::Keyboard::Key::Enter);
-    INPUT_KEY_TABLE[16] = (int)(sf::Keyboard::Key::LShift);
-    INPUT_KEY_TABLE[27] = (int)(sf::Keyboard::Key::Escape);
-    INPUT_KEY_TABLE[32] = (int)(sf::Keyboard::Key::Space);
-    INPUT_KEY_TABLE[34] = (int)(sf::Keyboard::Key::Quote);
-    INPUT_KEY_TABLE[37] = (int)(sf::Keyboard::Key::Left);
-    INPUT_KEY_TABLE[39] = (int)(sf::Keyboard::Key::Right);
-    INPUT_KEY_TABLE[44] = (int)(sf::Keyboard::Key::Comma);
-    INPUT_KEY_TABLE[45] = (int)(sf::Keyboard::Key::Hyphen);
-    INPUT_KEY_TABLE[46] = (int)(sf::Keyboard::Key::Period);
-    INPUT_KEY_TABLE[59] = (int)(sf::Keyboard::Key::Semicolon);
-    INPUT_KEY_TABLE[61] = (int)(sf::Keyboard::Key::Equal);
-    INPUT_KEY_TABLE[91] = (int)(sf::Keyboard::Key::LBracket);
-    INPUT_KEY_TABLE[92] = (int)(sf::Keyboard::Key::Backslash);
-    INPUT_KEY_TABLE[93] = (int)(sf::Keyboard::Key::RBracket);
-    INPUT_KEY_TABLE[126] = (int)(sf::Keyboard::Key::Tilde);
+    INPUT_KEY_TABLE[27] = (int)sf::Keyboard::Key::Escape;
+    INPUT_KEY_TABLE[17] = (int)sf::Keyboard::Key::LControl;
+    INPUT_KEY_TABLE[16] = (int)sf::Keyboard::Key::LShift;
+    INPUT_KEY_TABLE[18] = (int)sf::Keyboard::Key::LAlt;
+    INPUT_KEY_TABLE[17] = (int)sf::Keyboard::Key::RControl;
+    INPUT_KEY_TABLE[16] = (int)sf::Keyboard::Key::RShift;
+    INPUT_KEY_TABLE[18] = (int)sf::Keyboard::Key::RAlt;
+    INPUT_KEY_TABLE[93] = (int)sf::Keyboard::Key::Menu;
+    INPUT_KEY_TABLE[219] = (int)sf::Keyboard::Key::LBracket;
+    INPUT_KEY_TABLE[221] = (int)sf::Keyboard::Key::RBracket;
+    INPUT_KEY_TABLE[186] = (int)sf::Keyboard::Key::Semicolon;
+    INPUT_KEY_TABLE[188] = (int)sf::Keyboard::Key::Comma;
+    INPUT_KEY_TABLE[190] = (int)sf::Keyboard::Key::Period;
+    INPUT_KEY_TABLE[222] = (int)sf::Keyboard::Key::Quote;
+    INPUT_KEY_TABLE[191] = (int)sf::Keyboard::Key::Slash;
+    INPUT_KEY_TABLE[220] = (int)sf::Keyboard::Key::Backslash;
+    INPUT_KEY_TABLE[192] = (int)sf::Keyboard::Key::Tilde;
+    INPUT_KEY_TABLE[187] = (int)sf::Keyboard::Key::Equal;
+    INPUT_KEY_TABLE[189] = (int)sf::Keyboard::Key::Hyphen;
+    INPUT_KEY_TABLE[32] = (int)sf::Keyboard::Key::Space;
+    INPUT_KEY_TABLE[13] = (int)sf::Keyboard::Key::Enter;
+    INPUT_KEY_TABLE[8] = (int)sf::Keyboard::Key::Backspace;
+    INPUT_KEY_TABLE[9] = (int)sf::Keyboard::Key::Tab;
+    INPUT_KEY_TABLE[33] = (int)sf::Keyboard::Key::PageUp;
+    INPUT_KEY_TABLE[34] = (int)sf::Keyboard::Key::PageDown;
+    INPUT_KEY_TABLE[35] = (int)sf::Keyboard::Key::End;
+    INPUT_KEY_TABLE[36] = (int)sf::Keyboard::Key::Home;
+    INPUT_KEY_TABLE[45] = (int)sf::Keyboard::Key::Insert;
+    INPUT_KEY_TABLE[46] = (int)sf::Keyboard::Key::Delete;
+    INPUT_KEY_TABLE[107] = (int)sf::Keyboard::Key::Add;
+    INPUT_KEY_TABLE[109] = (int)sf::Keyboard::Key::Subtract;
+    INPUT_KEY_TABLE[106] = (int)sf::Keyboard::Key::Multiply;
+    INPUT_KEY_TABLE[111] = (int)sf::Keyboard::Key::Divide;
+    INPUT_KEY_TABLE[37] = (int)sf::Keyboard::Key::Left;
+    INPUT_KEY_TABLE[39] = (int)sf::Keyboard::Key::Right;
+    INPUT_KEY_TABLE[38] = (int)sf::Keyboard::Key::Up;
+    INPUT_KEY_TABLE[40] = (int)sf::Keyboard::Key::Down;
+    INPUT_KEY_TABLE[19] = (int)sf::Keyboard::Key::Pause;
+    INPUT_KEY_TABLE[189] = (int)sf::Keyboard::Key::Dash;
 }
 
-sf::Keyboard::Key ascii_to_key(int ascii_key) {
-    bool is_out_of_range = (ascii_key < 0) || (ascii_key > (TOTAl_INPUT_TABLE_SIZE - 1));
-
-    if (is_out_of_range) {
+sf::Keyboard::Key ascii_to_key(int key_code) {
+    if (key_code < 0 || key_code >= TOTAl_INPUT_TABLE_SIZE) {
+        // out of range
         return sf::Keyboard::Unknown;
+    } else {
+        return (sf::Keyboard::Key)(INPUT_KEY_TABLE[key_code]);
     }
-
-    return (sf::Keyboard::Key)(INPUT_KEY_TABLE[ascii_key]);
 }
 
-bool is_pressed(int ascii_key) {
-    sf::Keyboard::Key key = ascii_to_key(ascii_key);
-    return is_pressed(key);
-}
-
-bool is_pressed(sf::Keyboard::Key key) {
-    return sf::Keyboard::isKeyPressed(key);
+bool is_pressed(int key_code) {
+    return sf::Keyboard::isKeyPressed(ascii_to_key(key_code));
 }
 
 };
